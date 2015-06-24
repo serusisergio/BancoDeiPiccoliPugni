9e7c7a1f6507401f8fd6782de80ff000abf76d12
gcarl <gcarl@10.50.65.149>
2015-06-24 14:49:26 +0200

    Implemented  AuctionManager & UserManager

--- AsteOnline/src/asteonline/AuctionManager.java
+++ AsteOnline/src/asteonline/AuctionManager.java
@@ -25,27 +25,70 @@ public class AuctionManager {
         return instance;
     }
     
-    public AuctionManager() {
+    private AuctionManager() {
         this.auctions = new ArrayList<>();
     }
     
-    public void add(Auction a) {
-        this.auctions.add(a);
+    public int auctionsCount() {
+        return this.auctions.size();
     }
     
-    public List<Auction> find(UUID id) {
-        // find auction
-        // return results
-        return null;
+    public boolean add(Auction a) {
+        if(a == null)
+            throw new IllegalArgumentException();
+        
+        boolean result = this.auctions.add(a);
+        return result;
     }
     
-    public void remove(UUID id) {
-        //find auction
-        // remove from auctions
+    public Auction find(UUID id) {
+        if(id == null)
+            throw new IllegalArgumentException();
+        
+        Auction result = null;
+        for (Auction auction : this.auctions) {
+            if (auction.getId().equals(id)) {
+                result = auction;
+            }
+        }
+        return result;
     }
     
-    public void makeOffer(UUID dest, Offer o) {
-        //find auction
-        // add offer
+    public boolean remove(UUID id) {
+        if(id == null)
+            throw new IllegalArgumentException();
+        
+        Auction selectedAuction = null;
+        for (Auction auction : this.auctions) {
+            if (auction.getId().equals(id)) {
+                selectedAuction = auction;
+            }
+        }
+        
+        boolean result = false;
+        if(selectedAuction != null) {
+            result = this.auctions.remove(selectedAuction);
+        }
+        
+        return result;
+    }
+    
+    public boolean makeOffer(UUID dest, Offer o) {
+        if(dest == null || o == null)
+            throw new IllegalArgumentException();
+        
+        int index = -1;
+        for (Auction auction : this.auctions) {
+            if (auction.getId().equals(dest)) {
+                index = this.auctions.indexOf(auction);
+            }
+        }
+        
+        boolean res = false;
+        if(index >= 0) {
+            res = this.auctions.get(index).getOffers().add(o);
+        }
+
+        return res;
     }
 }
---
+++ AsteOnline/src/asteonline/UserManager.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteonline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author gcarl
+ */
+public class UserManager {
+    private static UserManager instance = null;   
+    private static final Object sync = new Object();
+    private List<User> users;
+    
+    public static UserManager getInstance(){
+        synchronized(sync) {
+            if(instance == null) instance = new UserManager();
+        }
+        return instance;
+    }
+    
+    private UserManager() {
+        this.users = new ArrayList<>();
+    }
+    
+    public boolean add(User u) {
+        if(u == null)
+            throw new IllegalArgumentException();
+        
+        boolean res;
+        res = this.users.add(u);
+        
+        return res;
+    }
+    
+    public Boolean remove(UUID id) {
+        if(id == null)
+            throw new IllegalArgumentException();
+        
+        User userResult = null;
+        for (User usersItem : this.users) {
+            if (usersItem.getId().equals(id)) {
+                userResult = usersItem;
+            }
+        }
+        
+        boolean res = false;
+        if(userResult != null)
+            res = this.users.remove(userResult);
+        
+        return res;
+    }
+    
+    public void login() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void logout() {
+        throw new UnsupportedOperationException();
+    }
+}
