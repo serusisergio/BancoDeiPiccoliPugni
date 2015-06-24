74877715a7884c128fc067dc7fc15f1bce539f44
Stefano Lande <landeste@gmail.com>
2015-06-24 18:05:43 +0200

    Added methods to bid items in Offer

--- AsteOnline/src/asteonline/Offer.java
+++ AsteOnline/src/asteonline/Offer.java
@@ -15,7 +15,7 @@ public class Offer {
     /**
      * Instantiate an offer with the parameters passed
      * @param amount the money amount to offer. 
-     * @param owner 
+     * @param owner the user making an offer
      */
     public Offer(float amount, User owner)
     {
@@ -23,15 +23,43 @@ public class Offer {
         this.offerOwner = owner;
         this.items = new ArrayList<>();
     }
-
+    
+    /**
+     * Adds a single item to the offer
+     * @param item the item to add
+     */
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+    
+    /**
+     * Adds a list of items to the offer
+     * @param list the list of items to add
+     */
+    public void addItem(List<Item> list){
+        this.items.addAll(list);
+    }
+    
+    /**
+     * Returns the amount of money being offered
+     * @return the amout of money
+     */
     public float getMoney() {
         return money;
     }
-
+    
+    /**
+     * Return the list of items being offered
+     * @return the list of items
+     */
     public List<Item> getItems() {
         return items;
     }
-
+    
+    /**
+     * Returns the owner of the offer
+     * @return the owner of the offer
+     */
     public User getOfferOwner() {
         return offerOwner;
     }
