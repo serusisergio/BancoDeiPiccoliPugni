729da7467d6d94efd11c1e79b58c87b0c28c343d
gcarl <gcarl@10.50.65.149>
2015-06-24 15:29:56 +0200

    rc version

--- AsteOnline/src/asteonline/Transaction.java
+++ AsteOnline/src/asteonline/Transaction.java
@@ -6,8 +6,6 @@
 package asteonline;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Objects;
 
 /**
  *
@@ -15,10 +13,10 @@ import java.util.Objects;
  */
 public class Transaction {
 
-    private User user;
-    private Auction auction;
-    private LocalDateTime date;
-    private Offer offer;
+    private final User user;
+    private final Auction auction;
+    private final LocalDateTime date;
+    private final Offer offer;
 
     public Transaction(User user, Auction auction, Offer offer) {
         this.auction = auction;
@@ -27,61 +25,19 @@ public class Transaction {
         this.offer=offer;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Transaction other = (Transaction) obj;
-        if (!Objects.equals(this.user, other.user)) {
-            return false;
-        }
-        if (!Objects.equals(this.auction, other.auction)) {
-            return false;
-        }
-        if (!Objects.equals(this.date, other.date)) {
-            return false;
-        }
-        return true;
-    }
-
     public User getUser() {
         return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 
     public Auction getAuction() {
         return auction;
     }
 
-    public void setAuction(Auction auction) {
-        this.auction = auction;
-    }
-
     public LocalDateTime getDate() {
         return date;
     }
 
-
-
-    /**
-     * @return the offer
-     */
     public Offer getOffer() {
         return offer;
     }
-
-   
 }
